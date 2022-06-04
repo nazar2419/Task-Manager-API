14527118d59e5e5adba66b0d40e73c87778676b6
nazar2419 <53973528+nazar2419@users.noreply.github.com>
2022-06-04 16:47:34 +0300

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectionString =
-  'mongodb+srv://nazar:111222333@nodeexspressprojects.mi4oc.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority'
+  ''
 
   const connectDB = (url) => {
     return mongoose.connect(connectionString)
