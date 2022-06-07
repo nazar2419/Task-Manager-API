4a9001b67ee05802ea7d3270bf3bc1b9b9cf356d
nazar2419 <nazarx095@gmail.com>
2022-06-07 12:52:47 +0300

    async wrapper

--- app.js
+++ app.js
@@ -3,22 +3,16 @@ const app = express();
 const tasks = require('./routers/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
 
 // middleware
-
+app.use(express.static('./public'));
 app.use(express.json());
 
 //routes 
-app.get('/hello', (req, res) => {
-  res.send('Task Manager App')
-})
 app.use('/api/v1/tasks', tasks);
 
-// app.get('/api/v1/tasks')   - get all the tasks
-// app.post('/api/v1/tasks')   - create a new task
-// app.get('/api/v1/tasks/:id')   - get single task
-// app.patch('/api/v1/tasks/:id')   - update task
-// app.delete('/api/v1/tasks/:id')   - delete task
+app.use(notFound);
 
 
 const port = 3000;
